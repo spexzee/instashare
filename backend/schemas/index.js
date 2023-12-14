94a974f17280d9b0e8eff6d8927a4bef6d465637
spexzee <abub5979@gmail.com>
2023-12-14 17:13:15 +0530

    added new schemas

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,4 +1,9 @@
 import user from './user';
+import pin from './pin';
+import postedBy from './postedBy';
+import save from './save';
+import comment from './comment';
+
 export const schemaTypes = [
-    user
+    user, pin, comment, postedBy ,save
 ]
